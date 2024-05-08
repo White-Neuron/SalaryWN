127b07be0a978f2cfb2c6d677a770d19ecc6d430
phnthqnh <qphan204@gmail.com>
2024-05-08 09:38:36 +0700

    fix gio lam

--- Payroll/admin.py
+++ Payroll/admin.py
@@ -100,7 +100,7 @@ class GioLamAmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
-    def has_delete_permission(self, request):
+    def has_delete_permission(self, request, obj=None):
         return False
 
 admin.site.register(GioLam, GioLamAmin)
